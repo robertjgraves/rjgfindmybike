b4782b6ae6cbb693037455b1a1e69a0c2917928c
Robert Graves <robertjgraves@gmail.com>
2013-11-15 13:44:02 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rjgfindmybike::Application.config.secret_key_base = 'd8cb2575b5e23a6f9832198585f88f785d6e2870a26bd99e9985d8a09e961b6d522912689fc7d8d841b7fbfab93cd6caffa92f8c78f85169e7730af87abd7091'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rjgfindmybike::Application.config.session_store :cookie_store, key: '_rjgfindmybike_session'
